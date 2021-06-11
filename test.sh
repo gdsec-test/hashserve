47f8cffabac95858d0337b010d28ef41917cc8a6
Dustin <dmcallister1@godaddy.com>
2021-06-11 11:37:22 -0400

    run a bash script in gh actions

---
+++ test.sh
@@ -0,0 +1,6 @@
+	COMPLETE_COUNT=$(kubectl --context dev-dcu -n abuse-api-dev get pods | grep -i shadowfax-integration-test | grep -i completed | wc -l | xargs );\
+	if [ "$$COMPLETE_COUNT" = "1" ]; then \
+    echo "Integration test passed successfully"; \
+	else \
+	  echo "Integration tests failed!!!!! Revert changes manually."; \
+	fi;
