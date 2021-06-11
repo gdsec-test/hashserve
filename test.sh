f322c635609789f01c1fe7e3cfa365add9ee3699
Dustin <dmcallister1@godaddy.com>
2021-06-11 15:59:37 -0400

    kubectl change

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-	COMPLETE_COUNT=$(kubectl --context dev-dcu -n abuse-api-dev get pods | grep -i shadowfax-integration-test | grep -i completed | wc -l | xargs );\
+	COMPLETE_COUNT=$(/var/github-actions/_work/_tool/kubectl/1.15.0/x64/kubectl --context dev-dcu -n abuse-api-dev get pods | grep -i shadowfax-integration-test | grep -i completed | wc -l | xargs );\
 	if [ "$$COMPLETE_COUNT" = "1" ]; then \
     echo "Integration test passed successfully"; \
 	else \
