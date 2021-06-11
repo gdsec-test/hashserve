395911377a2dd7355ada107208fffcfab0d643e3
Dustin <dmcallister1@godaddy.com>
2021-06-11 16:42:00 -0400

    bash fixes

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-	COMPLETE_COUNT=$(/var/github-actions/_work/_tool/kubectl/1.15.0/x64/kubectl --context dev-dcu -n abuse-api-dev get pods | grep -i shadowfax-integration-test | grep -i completed | wc -l | xargs );\
-	if [ "$$COMPLETE_COUNT" = "1" ]; then \
-    echo "Integration test passed successfully"; \
-	else \
-	  echo "Integration tests failed!!!!! Revert changes manually.";
-	   exit 1
-	fi;
+COMPLETE_COUNT=$(kubectl --context dev-dcu -n abuse-api-dev get pods | grep -i shadowfax-integration-test | grep -i completed | wc -l | xargs );\
+if [ "$COMPLETE_COUNT" = "1" ]; then
+echo "Integration test passed successfully";
+else
+	echo "Integration tests failed!!!!! Revert changes manually.";
+	exit 1
+fi;
